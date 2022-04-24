95b2951963b716b15aae5e6bec432012e8bd882a
Dillon Roth <dillon.roth11@gmail.com>
2022-04-23 17:41:13 -0700

    models

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const ReactionSchema = require('./Reaction');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: [1, 'Not enough characters'],
+        maxlength: [280, 'Too many characters']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+}
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
